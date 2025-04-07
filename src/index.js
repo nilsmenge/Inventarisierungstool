28bd4f2d3cdec9effaa9fcc7ab036760ed84fef5
Nils Menge <menge.nils@gmx.de>
2025-04-07 10:09:18 +0200

    Delete src directory

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
-
-const element = <h1>Hello, world!</h1>;
-const root = ReactDOM.createRoot(document.getElementById('root')); // Create a root
-root.render(element); // Render to the root
