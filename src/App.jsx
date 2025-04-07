a8676828b95fb39a9d615d7b3db7f2fa7dd7bdc3
Nils Menge <menge.nils@gmx.de>
2025-04-07 10:06:28 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Landingpage from './Components/Landingpage/Landingpage'
 
 const App = () => {
   return (
     <div className='container'>
-      <Navbar/>
+      <Landingpage/>
     </div>
   )
 }
