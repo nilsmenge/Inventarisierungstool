5de0c7e69d56d438d0377d60d8245eb87032fbec
Nils Menge <menge.nils@gmx.de>
2025-04-08 11:40:11 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Landingpage from './Components/Landingpage/Landingpage'
 import QrReader from './Components/Scanner/QrScanner'
+import Login from './Components/Login/Login'
 
 const App = () => {
   return (
     <div className='container'>
-      <QrReader/>
+      <Login/>
+      {/*<QrReader/>*/}
       {/*<Landingpage/>*/}
     </div>
   )
