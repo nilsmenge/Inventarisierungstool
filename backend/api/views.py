1d7b21712c5e50fd7cd9ec3faa34b87115706c9e
MoSchaefer <m19.schaefer@gmail.com>
2025-05-21 23:44:27 +0200

    user_detail api

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,12 +6,17 @@ from .serializer import UserSerializer
 
 
 @api_view(['GET'])
-def get_user(request):
+def get_users(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+    '''
     return Response(UserSerializer({'last_name': "user", 
                                     'first_name': "test", 
                                     'department': "OE17", 
                                     'email': "tuser@inventool.com", 
                                     'password': "abc123"}).data)
+    '''
 
 @api_view(['POST'])
 def create_user(request):
@@ -21,3 +26,25 @@ def create_user(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
