b1e19bad75efd8b1bd451859843143f5f606753b
MoSchaefer <m19.schaefer@gmail.com>
2025-05-23 11:08:17 +0200

    new api assets

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import get_users, create_user, user_detail
+from .views import get_users, create_user, user_detail, get_assets, create_asset
 
 urlpatterns = [
     path('users/', get_users, name='get_users'),
     path('users/create/', create_user, name='create_user'),
-    path('users/<int:pk>/', user_detail, name='user_detail')
+    path('users/<int:pk>/', user_detail, name='user_detail'),
+    path('assets/', get_assets, name='get_assets'),
+    path('assets/create/', create_asset, name='create_asset')
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import User
-from .serializer import UserSerializer
+from .models import User, Asset
+from .serializer import UserSerializer, AssetSerializer
 
 
 @api_view(['GET'])
@@ -10,13 +10,13 @@ def get_users(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
-    '''
-    return Response(UserSerializer({'last_name': "user", 
-                                    'first_name': "test", 
-                                    'department': "OE17", 
-                                    'email': "tuser@inventool.com", 
-                                    'password': "abc123"}).data)
-    '''
+    
+    #return Response(UserSerializer({'last_name': "user", 
+    #                                'first_name': "test", 
+    #                                'department': "OE17", 
+    #                                'email': "tuser@inventool.com", 
+    #                                'password': "abc123"}).data)
+    
 
 @api_view(['POST'])
 def create_user(request):
@@ -48,3 +48,22 @@ def user_detail(request, pk):
     elif request.method == 'DELETE':
         user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET'])
+def get_assets(request):
+    assets = Asset.objects.all()
+    serializer = AssetSerializer(assets, many=True)
+    return Response(serializer.data)
+
+    #return Response(AssetSerializer({'serial_no': "123456789", 
+    #                                'device_name': "test", 
+    #                                'category': "Laptop"}).data)
+
+@api_view(['POST'])
+def create_asset(request):
+    data = request.data
+    serializer = AssetSerializer(data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
