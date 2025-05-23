e46a501e06b98b1af945f2dd0a63a965d03b7bfa
MoSchaefer <m19.schaefer@gmail.com>
2025-05-23 11:28:21 +0200

    assets_detail api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import get_users, create_user, user_detail, get_assets, create_asset
+from .views import get_users, create_user, user_detail, get_assets, create_asset, asset_detail
 
 urlpatterns = [
     path('users/', get_users, name='get_users'),
     path('users/create/', create_user, name='create_user'),
     path('users/<int:pk>/', user_detail, name='user_detail'),
     path('assets/', get_assets, name='get_assets'),
-    path('assets/create/', create_asset, name='create_asset')
+    path('assets/create/', create_asset, name='create_asset'),
+    path('assets/<int:pk>/', asset_detail, name='asset_detail')
 ]
