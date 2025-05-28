2d6d398d35231e044cf8528cf6d266b952b3aa09
MoSchaefer <m19.schaefer@gmail.com>
2025-05-28 19:53:36 +0200

    2. settings change

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,7 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://https://main.d2lll7pho2g9zs.amplifyapp.com/"
+    "http://https://main.d2lll7pho2g9zs.amplifyapp.com"
 ]
 
 # Configure Django App for Heroku.
