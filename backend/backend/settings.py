eaa7d9222ce1ee3c32a903662eae5015d4cd349f
MoSchaefer <m19.schaefer@gmail.com>
2025-05-28 16:21:26 +0200

    init heroku

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
