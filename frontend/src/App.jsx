2c48b47ee9a800e40557869a8da5ca6a8c53a545
Nils Menge <menge.nils@gmx.de>
2025-06-02 14:23:40 +0200

    init

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './Components/Navbar/Navbar'
 import Landingpage from './Components/Landingpage/Landingpage'
 import QrBarcodeScanner from './Components/Scanner/QrScanner'
 import Login from './Components/Login/Login'
-import Dashboard from './Components/AdminDashboard/AdminDashboard'
+import AdminDashboard from './Components/AdminDashboard/AdminDashboard'
 import Navigator from './Components/Navigator/Navigator'
 import Inventory from './Components/Inventory/Inventory'
 import AssetManager from './Components/AssetManager/AssetManager'
@@ -17,7 +17,7 @@ const App = () => {
         <Route path='/' element={<Landingpage />} />
         <Route path='/login' element={<Login />} />
         <Route path='/scanner' element={<QrBarcodeScanner />} />
-        <Route path='/admindashboard' element={<Dashboard />} />
+        <Route path='/admindashboard' element={<AdminDashboard />} />
         <Route path='/navigator' element={<Navigator />} />
         <Route path='/inventory' element={<Inventory />} />
         <Route path='/assets' element={<AssetManager />} />
