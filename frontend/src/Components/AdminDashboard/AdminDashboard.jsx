430e31180cc4f67238d5bded409c07ccfceea9e3
Nils Menge <menge.nils@gmx.de>
2025-06-02 14:18:33 +0200

    init

--- frontend/src/Components/AdminDashboard/AdminDashboard.jsx
+++ frontend/src/Components/AdminDashboard/AdminDashboard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Eye, EyeOff } from "lucide-react";
-import "./Dashboard.css";
+import "./AdminDashboard.css";
 import {
   Icon,
   Search,
